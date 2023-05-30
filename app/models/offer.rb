0808a9ca04795ae7efb0d12fd9ab47dd38fe422f
Karen Kintrup <karenkintrup@gmail.com>
2023-05-30 12:25:11 +0200

    models - added the associations and validations

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,4 +1,5 @@
 class Offer < ApplicationRecord
   belongs_to :project
   belongs_to :user
+
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,13 @@
 class Project < ApplicationRecord
   belongs_to :user
+  has_many :offers
+
+  validates :room, presence: true
+  validates :location, presence: true
+  validates :budget, presence: true
+  validates :time_frame, presence: true
+  validates :sustainability, presence: true
+  validates :description, presence: true
+  validates :img_url, presence: true
+
 end
