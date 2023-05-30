ff380678c4e993e569f0dc57f0b04dee806792e8
Rahul Salotra <rs.salotra@gmail.com>
2023-05-30 16:04:34 +0200

    projects new and create

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,7 +2,7 @@ class ProjectsController < ApplicationController
 
 def index
   # filled by denise
-end 
+end
 
 def show
   # filled by karen
@@ -10,10 +10,20 @@ end
 
 def new
   # filled by rahul
+  @project = Project.new
 end
 
 def create
   # filled by rahul
+  @user = current_user
+  @project = Project.new(project_params)
+  @project.user = @user
+  @project.save
+  redirect_to projects_path
 end
+private
 
+def project_params
+  params.require(:project).permit([:room, :location, :budeget, :time_frame, :sustainability, :description, :img_url])
+end
 end
