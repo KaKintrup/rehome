a4ebccd9d1a323df332b36e04bc2b1dc5d0feaed
Rahul Salotra <rs.salotra@gmail.com>
2023-05-31 11:28:04 +0200

    added delete and edit and fixed a bug

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,8 +1,8 @@
 class ProjectsController < ApplicationController
   def index
-    @projects = Projects.all
+    @projects = Project.all
   end
-  
+
   def show
     @project = Project.find(params[:id])
   end
@@ -18,10 +18,26 @@ class ProjectsController < ApplicationController
     @project.save
     redirect_to projects_path
   end
- 
- private
+
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def update
+    @project = Project.find(params[:id])
+    @project.update(project_params)
+    redirect_to project_path(@project)
+  end
+
+  def destroy
+    @project = Project.find(params[:id])
+    @project.destroy
+    redirect_to projects_path, status: :see_other
+  end
+
+  private
 
   def project_params
-    params.require(:project).permit([:room, :location, :budeget, :time_frame, :sustainability, :description, :img_url])
-   end
- end
+    params.require(:project).permit([:room, :location, :budget, :time_frame, :sustainability, :description, :img_url])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :projects, only: [:index, :show, :new, :create]
+  resources :projects
 end
