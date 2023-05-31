5a6821a62ef72f068f3b8da75ae183a2f7e5148a
Karen Kintrup <karenkintrup@gmail.com>
2023-05-31 14:33:43 +0200

    created map for homepage and added some general styling

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,11 @@
 class PagesController < ApplicationController
   def home
+    @projects = Project.all
+    @markers = @projects.geocoded.map do |project|
+      {
+        lat: project.latitude,
+        lng: project.longitude
+      }
+    end
   end
 end
